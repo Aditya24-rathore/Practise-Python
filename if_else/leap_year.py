13f1cf7221b02364015c9a5d4d4ad406e71d2ec0
Adityaraj Rathore <ra4402538@gmail.com>
2025-06-17 07:50:02 -0700

    new

--- if_else/leap_year.py
+++ if_else/leap_year.py
@@ -3,3 +3,5 @@ if year%4==0 and year%400!=0:
     print(year,"is a leap year")
 else:
     print(year,"is not a leap year")
+
+    
---
+++ if_else/signal.py
@@ -0,0 +1,9 @@
+color=str(input("Enter color"))
+if color=="green" or color=="Green":
+    print("you can go")
+elif color=="red" or color=="Red":
+    print("you can stop")
+elif color=="yellow" or color=="Yellow":
+    print("Slow Down")
+else:
+    print("enter valid color")
