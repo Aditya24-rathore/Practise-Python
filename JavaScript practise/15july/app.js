70cf7acd10bf9ad1592932e43601ba69982602c8
Adityaraj Rathore <ra4402538@gmail.com>
2025-07-15 06:31:27 -0700

    DOM

---
+++ JavaScript practise/15july/app.js
@@ -0,0 +1,19 @@
+function change1() {
+    let green=document.querySelector("#changeColor")
+    green.style.backgroundColor="Green"
+}
+
+function change2() {
+    let green=document.querySelector("#changeColor")
+    green.style.backgroundColor="Red"
+}
+
+function change3() {
+    let green=document.querySelector("#changeColor")
+    green.style.backgroundColor="Blue"
+}
+
+function clickChange(){
+    let click1=document.querySelector("#click")
+    click1.style.color="Red"
+}
