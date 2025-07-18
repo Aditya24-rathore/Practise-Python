122787d6dc611ef5b2430200c06cda720a44bc2d
Adityaraj Rathore <ra4402538@gmail.com>
2025-07-18 07:11:10 -0700

    updated

--- JavaScript practise/18july/app.js
+++ JavaScript practise/18july/app.js
@@ -1,3 +1,12 @@
 let fruits=["apple","banana","cherry","mango"]
 let fruit=fruits.splice(0,3,2)
 console.log(fruit)
+
+let arr=[-3,10,10,-10,10]
+sum=0
+for(let num of arr){
+    if(num>0){
+      sum=sum+num
+    }
+}
+console.log(sum)
